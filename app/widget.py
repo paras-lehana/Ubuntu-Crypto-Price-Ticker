e1a3b2a27fa95dcebcea74c530d8b518c2d30ccd
Paras Lehana <paras.lehana@indiamart.com>
2019-04-19 10:23:12 +0530

    adding support for precision (custom price decimal places)

--- app/widget.py
+++ app/widget.py
@@ -24,7 +24,8 @@ class Widget(object):
         conf = token.split(',')
 
         self.token = conf[0].lower()
-        self.convert = conf[1].lower() if len(conf) > 1 else 'usd'
+        self.convert = conf[1].lower() if len(conf) > 1 and len(conf[1]) else 'usd'
+        self.precision = int(conf[2].lower()) if len(conf) > 2 and len(conf[2]) else 2
         self.widget = AppIndicator3.Indicator.new(
             "Ubuntu Crypto Price Ticker {}".format(self.token),
             self.icon, 
@@ -57,10 +58,9 @@ class Widget(object):
 
         token_key = 'price_btc' if self.convert == 'sat' else 'price_' + str(self.convert)
         amount = float(token_data[token_key]) * (100000000 if self.convert == 'sat' else 1)
+        display_price =  int(round(float(amount), self.precision)) if self.precision == 0 else round(float(amount), self.precision)
         price_string = ' {}{}'.format(
-            currency_symbol.get(self.convert, ''),
-            int(round(float(amount), 0))
-        )
+            currency_symbol.get(self.convert, ''),display_price)
 
         self.widget.set_label(price_string, self.token)
 
